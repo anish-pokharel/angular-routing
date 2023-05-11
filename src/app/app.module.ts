378aa140e2cebb472e50c3019dd033e6b5bc5fd2
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-05-11 14:38:35 +0545

    roting concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { CategoriesComponent } from './categories/categories.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },   //localhost:4200
+  { path: "users", component: UserComponent },
+  { path: "categories", component: CategoriesComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +24,8 @@ import { CategoriesComponent } from './categories/categories.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
